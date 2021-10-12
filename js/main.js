2ffba6c7234b25fdf240b747d70b9309d71040d5
Bravehearts19 <leo18111998@gmail.com>
2021-10-12 15:59:39 +0200

    MIglioramenti nel layout: manca centratura degli span e passaggio a 7 cols

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const container = document.querySelector(".container-fluid");
+const containerFluid = document.querySelector(".container-fluid");
+
+const container = document.createElement("div");
+container.classList.add("container");
 
 const row = document.createElement("div");
-row.classList.add("row", "g-3", "row_h", "flex-wrap");
+row.classList.add("row", "g-3", "flex-wrap", "row-cols-6");
 
+containerFluid.append(container);
 container.append(row);
 
 for (let i = 1; i <= 100; i++) {
@@ -10,26 +14,27 @@ for (let i = 1; i <= 100; i++) {
     let text = "";
 
     if (i % 3 === 0 && i % 5 !== 0) {
-        bgColor = "bg_fizz";
+        bgColor = "bg_color_fizz";
         text = "fizz";
     } else if (i % 3 !== 0 && i % 5 === 0) {
-        bgColor = "bg_buzz";
+        bgColor = "bg_color_buzz";
         text = "buzz";
     } else if (i % 3 === 0 && i % 5 === 0) {
-        bgColor = "bg_fizz_buzz";
+        bgColor = "bg_color_fizz_buzz";
         text = "fizzbuzz";
     } else {
-        bgColor = "bg_standard";
+        bgColor = "bg_color_standard";
         text = `${i}`;
     }
 
     const col = document.createElement("div");
-    col.classList.add("col", "col_w_and_h");
+    col.classList.add("col"/* , "col_w_and_h" */);
 
     const square = document.createElement("div");
-    square.classList.add("ratio", "ratio-1x1", "text-center", "w-100", bgColor);
+    square.classList.add("ratio", "ratio-1x1", "text-center", "w-100", "postion-relative", bgColor);
 
     const squareText = document.createElement("span");
+    squareText.classList.add("span_center")
     squareText.textContent = text;
 
     row.append(col);
